a787e909e3ca148896494595d0dbc467698bcf17
nasirrhh <nasir.hussain786505@gmail.com>
2023-08-16 12:41:02 +0530

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# TypeScript Mern Anyway Carpet
+
+## Lessons
+
+1. Introduction
+2. Install tools
+3. Create typescript react appp by vite
+4. Create git repository
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <header>
+      Anyway Carpet
+    </header>
+    <main></main>
+    <footer>All right reserved</footer>
+   </div>
+  )
+}
+
+export default App
